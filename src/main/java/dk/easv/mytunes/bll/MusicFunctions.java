a95d854bfe007d3b58b0c0e8b12131ca883baf8c
DESKTOP-V4I32ES\Dada <david.terek2005@gmail.com>
2025-11-25 14:05:10 +0100

    MusicFunctions updated with duration and names.

--- src/main/java/dk/easv/mytunes/bll/MusicFunctions.java
+++ src/main/java/dk/easv/mytunes/bll/MusicFunctions.java
@@ -4,9 +4,12 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class MusicFunctions {
-    String bip = "music.mp3";
-    Media hit = new Media(new File(bip).toURI().toString());
-    MediaPlayer mediaPlayer = new MediaPlayer(hit);
+    private String bip = "music.mp3";
+    private Media hit = new Media(new File(bip).toURI().toString());
+    private MediaPlayer mediaPlayer = new MediaPlayer(hit);
+    //private String duration = mediaPlayer.getTotalDuration().toString();
+    //private String durationTrimed = duration.replace("ms", "").trim();
+    //private int durationInt = Integer.parseInt(durationTrimed);
 
     public void playMusic() {
         mediaPlayer.play();
@@ -20,7 +23,28 @@ public class MusicFunctions {
          * with the same button
          */
         return mediaPlayer.getStatus().toString();
+    }
 
+    public void setVolume(int volume) {
+        mediaPlayer.setVolume(volume);
+    }
+    public String getDuration() {
+        /**Duration in in ms and must be converted to string to cut of the "ms" part
+         * then into double because the duration of the last song was x.0 so it cannot be converted into int from string
+         * therefor it must be double and then integer
+         */
+        String duration = mediaPlayer.getTotalDuration().toString().replace("ms", "").trim();
+        double durationDouble = Double.parseDouble(duration);
+        int durationInt = (int) durationDouble;
+        int totalseconds = durationInt / 1000;
+        int minutes = totalseconds / 60;
+        int seconds = totalseconds % 60;
+        String fullDuration = String.format("%02d:%02d", minutes, seconds);
+
+        return fullDuration;
+    }
+    public String getMusic() {
+        return bip.replace(".mp3", "").trim();
     }
 
 }
--- src/main/java/dk/easv/mytunes/gui/MyTunesController.java
+++ src/main/java/dk/easv/mytunes/gui/MyTunesController.java
@@ -2,16 +2,31 @@ package dk.easv.mytunes.gui;
 
 import dk.easv.mytunes.bll.*;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 
 public class MyTunesController {
+    @FXML
+    private Label lblName;
+    @FXML
+    private Label lblDuration;
+    @FXML
+    private Slider sliderVolume;
+
     MusicFunctions musicFunctions = new MusicFunctions();
     public void btnPlayOnClick(ActionEvent actionEvent) {
+
+        lblDuration.setText(musicFunctions.getDuration());
+        lblName.setText(musicFunctions.getMusic());
         if (musicFunctions.getStatus().equals("PLAYING")) {
             musicFunctions.pauseMusic();
+
         } else {
             musicFunctions.playMusic();
+
         }
     }
+
+
 }
